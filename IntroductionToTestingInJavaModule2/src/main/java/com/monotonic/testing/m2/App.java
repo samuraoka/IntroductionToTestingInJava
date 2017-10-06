bb1e1004d70a1391d7ff99d4b2bf3b68e4517729
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-10-06 16:26:11 +0900

    Introduction to Testing in Java
    Writing Your First Test - initial commit.

---
+++ IntroductionToTestingInJavaModule2/src/main/java/com/monotonic/testing/m2/App.java
@@ -0,0 +1,13 @@
+package com.monotonic.testing.m2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
