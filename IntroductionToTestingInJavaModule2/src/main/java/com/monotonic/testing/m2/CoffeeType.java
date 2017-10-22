1562f6610a4c9fe737422eeecded4b1cad91fd8c
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-10-22 20:57:58 +0900

    changed required beans in espresso

--- IntroductionToTestingInJavaModule2/src/main/java/com/monotonic/testing/m2/CoffeeType.java
+++ IntroductionToTestingInJavaModule2/src/main/java/com/monotonic/testing/m2/CoffeeType.java
@@ -4,7 +4,7 @@ package com.monotonic.testing.m2;
  * Describes the type of coffee that we're going to serve.
  */
 public enum CoffeeType {
-	Espresso(7, 0), Latte(7, 227), FilterCoffee(10, 0);
+	Espresso(8, 0), Latte(7, 227), FilterCoffee(10, 0);
 
 	private final int requiredBeans;
 	private final int requiredMilk;
