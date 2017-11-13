426cab1ed44e5ba6d66293bea33135b8ad3c4b75
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-14 04:13:51 +0900

    refactored isLeap method

--- IntroductionToTestingInJavaModule4/src/main/java/com/monotonic/testing/m4/LeapYear.java
+++ IntroductionToTestingInJavaModule4/src/main/java/com/monotonic/testing/m4/LeapYear.java
@@ -3,7 +3,15 @@ package com.monotonic.testing.m4;
 public class LeapYear {
 
 	public static boolean isLeapYear(int year) {
-		return year % 4 == 0 && !(year % 100 == 0);
+		return isDivisible(year, 4) && isNotDivisible(year, 100);
+	}
+
+	private static boolean isDivisible(int year, int denominator) {
+		return year % denominator == 0;
+	}
+	
+	private static boolean isNotDivisible(int year, int denominator) {
+		return !isDivisible(year, denominator);
 	}
 
 }
