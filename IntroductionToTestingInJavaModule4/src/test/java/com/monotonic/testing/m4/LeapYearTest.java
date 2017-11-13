897b2d034c90e4f7d5309d8678889a4944c5cc91
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-14 04:05:29 +0900

    added a new test case

--- IntroductionToTestingInJavaModule4/src/test/java/com/monotonic/testing/m4/LeapYearTest.java
+++ IntroductionToTestingInJavaModule4/src/test/java/com/monotonic/testing/m4/LeapYearTest.java
@@ -1,5 +1,6 @@
 package com.monotonic.testing.m4;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,10 +10,15 @@ public class LeapYearTest {
 	// a year is a leap year if it is divisible by four
 	// but, years divisible by 100, are not leap years.
 	// except year divisible by 400
-	
+
 	@Test
 	public void LeapYearsAreDivisibleByFour() {
 		assertTrue(LeapYear.isLeapYear(2020));
+	}
+
+	@Test
+	public void normalYearsAreNoDivisibleByFour() {
+		assertFalse(LeapYear.isLeapYear(2021));
 	}
 
 }
