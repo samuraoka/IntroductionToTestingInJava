629922250804dfe16ed8e1c99762f54c518bfc87
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-12-09 10:00:32 +0900

    implemented com.monotonic.testing.m5.SalesReport

---
+++ IntroductionToTestingInJavaModule5/src/main/java/com/monotonic/testing/m5/SalesAnalysisService.java
@@ -0,0 +1,22 @@
+package com.monotonic.testing.m5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SalesAnalysisService {
+
+	public SalesAnalysisService(String fileLocation) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Map<String, Integer> tallyStoreSales() {
+		// TODO Auto-generated method stub
+		return new HashMap<>();
+	}
+
+	public Map<String, Integer> tallyProductSales() {
+		// TODO Auto-generated method stub
+		return new HashMap<>();
+	}
+
+}
--- IntroductionToTestingInJavaModule5/src/main/java/com/monotonic/testing/m5/SalesReport.java
+++ IntroductionToTestingInJavaModule5/src/main/java/com/monotonic/testing/m5/SalesReport.java
@@ -1,15 +1,35 @@
 package com.monotonic.testing.m5;
 
 import java.io.PrintStream;
+import java.time.LocalDate;
+import java.util.Map;
 
 public class SalesReport {
 
-	public SalesReport(PrintStream out, String string) {
-		// TODO Auto-generated constructor stub
+	private final PrintStream output;
+	private final SalesAnalysisService analyser;
+
+	public SalesReport(PrintStream output, String fileLocation) {
+		this.output = output;
+		this.analyser = new SalesAnalysisService(fileLocation);
 	}
 
 	public void report() {
-		// TODO Auto-generated method stub
-		
+		output.printf("%nSales Report for %s%n%n", LocalDate.now());
+
+		printTable("Store Sales", analyser.tallyStoreSales());
+		printTable("Product Sales", analyser.tallyProductSales());
+	}
+
+	private void printTable(String title, Map<String, Integer> values) {
+		output.printf("---------- %s ----------%n", title);
+		printRow(title, "Value");
+		values.forEach((key, value) -> printRow(key, "" + value));
+		output.println();
+		output.println();
+	}
+
+	private void printRow(String first, String second) {
+		output.printf("- %-15s - %6.6s -%n", first, second);
 	}
 }
