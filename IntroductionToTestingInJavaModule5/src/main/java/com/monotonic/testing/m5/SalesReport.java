b4e2a1e3fd2367febae44047ab040646c5dbdaf6
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-12-09 09:29:19 +0900

    implemented com.monotonic.testing.m5.ReportRunner class

---
+++ IntroductionToTestingInJavaModule5/src/main/java/com/monotonic/testing/m5/SalesReport.java
@@ -0,0 +1,15 @@
+package com.monotonic.testing.m5;
+
+import java.io.PrintStream;
+
+public class SalesReport {
+
+	public SalesReport(PrintStream out, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void report() {
+		// TODO Auto-generated method stub
+		
+	}
+}
